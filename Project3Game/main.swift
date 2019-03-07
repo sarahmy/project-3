b60de9ee50eec19a87cf5b54d3eba3de7cdf34ca
Sarah <maimoun.sarah@gmail.com>
2019-03-07 17:07:27 +0100

    Initial Commit

---
+++ Project3Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Project3Game
+//
+//  Created by Sarah Maimoun on 07/03/2019.
+//  Copyright © 2019 Sarah Maimoun. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
